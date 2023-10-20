826b5d52a9fef287fabceff80bd84ebbc3eebd0f
muhammadbilal10 <muhdbilal81@gmail.com>
2023-10-20 14:53:12 +0500

    update folder structure

---
+++ app/page.jsx
@@ -0,0 +1,30 @@
+import { Box, Typography, Stack } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Box marginTop={15}>
+      <Stack alignItems="center">
+        <Typography align="center" variant="h2" fontWeight="bold">
+          Discover & Share
+        </Typography>
+        <Typography
+          variant="h2"
+          className="orange-gradient"
+          align="center"
+          component="span"
+          fontWeight="bold"
+          gutterBottom
+          color="primary"
+        >
+          AI Powered Prompts
+        </Typography>
+        <Typography variant="body1" align="center">
+          Promptia is an open-source AI prompting tool for modern world to
+          discover, create and share creative prompts.
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Home;
